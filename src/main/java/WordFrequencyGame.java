c6d01e60579e7e93b43efb8168a5c24b108ba771
zjx-immersion <jianxin_home@163.com>
2018-05-22 23:33:20 +0800

    [Jason] add a batch of smell code with clear test

---
+++ src/main/java/WordFrequencyGame.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+/**
+ * Created by jxzhong on 2018/5/22.
+ */
+public class WordFrequencyGame {
+    public String getResult(String inputStr) {
+
+        if (inputStr.split("\\s+").length == 1) {
+            return inputStr + " 1";
+        } else {
+
+            try {
+
+                String[] arr = inputStr.split("\\s+");
+
+                List<Input> inputList = new ArrayList<>();
+                for (String s : arr) {
+                    Input input = new Input(s, 1);
+                    inputList.add(input);
+                }
+
+                Map<String, List<Input>> map = getListMap(inputList);
+
+                List<Input> list = new ArrayList<>();
+                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+                    Input input = new Input(entry.getKey(), entry.getValue().size());
+                    list.add(input);
+                }
+                inputList = list;
+
+                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+
+                StringJoiner joiner = new StringJoiner("\n");
+                for (Input w : inputList) {
+                    String s = w.getValue() + " " + w.getWordCount();
+                    joiner.add(s);
+                }
+                return joiner.toString();
+            } catch (Exception e) {
+                return "Calculate Error";
+            }
+        }
+    }
+
+    private Map<String, List<Input>> getListMap(List<Input> inputList) {
+        Map<String, List<Input>> map = new HashMap<>();
+        for (Input input : inputList) {
+//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
+            if (!map.containsKey(input.getValue())) {
+                ArrayList arr = new ArrayList<>();
+                arr.add(input);
+                map.put(input.getValue(), arr);
+            } else {
+                map.get(input.getValue()).add(input);
+            }
+        }
+        return map;
+    }
+}
